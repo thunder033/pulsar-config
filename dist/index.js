45bd225bd18c1f115f297b6708062b74788c319f
Greg Rozmarynowycz <crashtest1@live.com>
2017-11-17 16:37:19 -0800

    re-organize index file for better exports

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,16 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 /**
  * Shared values and functionality for pulsar-api and pulsar-ui
  * Created by gjrwcs on 3/21/2017.
  */
-const enum_1 = require("./src/enum");
-exports.Enum = enum_1.Enum;
 const EventTypes = require("./src/event-types");
 exports.EventTypes = EventTypes;
 const GameParams = require("./src/game-params");
 exports.GameParams = GameParams;
-const priority_queue_1 = require("./src/priority-queue");
-exports.PriorityQueue = priority_queue_1.PriorityQueue;
-const Math = require("./src/math");
-exports.Math = Math;
+__export(require("./src/enum"));
+__export(require("./src/priority-queue"));
+__export(require("./src/math"));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -2,16 +2,11 @@
  * Shared values and functionality for pulsar-api and pulsar-ui
  * Created by gjrwcs on 3/21/2017.
  */
-import { Enum } from './src/enum';
 import * as EventTypes from './src/event-types';
 import * as GameParams from './src/game-params';
-import { PriorityQueue } from './src/priority-queue';
-import * as Math from './src/math';
 
-export {
-    EventTypes,
-    Enum,
-    GameParams,
-    PriorityQueue,
-    Math
-}
+export * from './src/enum';
+export * from './src/priority-queue';
+export * from './src/math';
+
+export {EventTypes, GameParams};
